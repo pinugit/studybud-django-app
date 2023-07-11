6bf23a4976ad167de48a48152645a698fe01efce
priyanshu <pinu@DESKTOP-D0TLML6>
2023-07-11 16:46:26 +0530

    added tamplates folder and add html to the links

--- studybud/settings.py
+++ studybud/settings.py
@@ -37,7 +37,7 @@ INSTALLED_APPS = [
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
-    "base"
+    "base.apps.BaseConfig"
 ]
 
 MIDDLEWARE = [
@@ -55,7 +55,9 @@ ROOT_URLCONF = "studybud.urls"
 TEMPLATES = [
     {
         "BACKEND": "django.template.backends.django.DjangoTemplates",
-        "DIRS": [],
+        "DIRS": [
+            BASE_DIR / "tamplates"
+        ],
         "APP_DIRS": True,
         "OPTIONS": {
             "context_processors": [
