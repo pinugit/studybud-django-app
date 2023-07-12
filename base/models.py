ca1f67e8915b2b451fef1543899508bdf667ca10
priyanshu <pinu@DESKTOP-D0TLML6>
2023-07-12 11:39:30 +0530

    add a room table(migration)

--- base/models.py
+++ base/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
 # Create your models here.
+
+class Room(models.Model):
+    name = models.CharField(max_length=200)
+    discription = models.TextField(null=True)
+    updated = models.DateTimeField(auto_now=True, blank=True)
+    saved = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self) -> str:
+        return self.name
+
